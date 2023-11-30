37ee7dbed4fdafce3804a9496f89e2d9854dceed
seonghun9906 <jsh990616@naver.com>
2023-11-30 10:04:18 +0900

    내일은 금요일

--- src/ex1/Example1.java
+++ src/ex1/Example1.java
@@ -5,6 +5,8 @@ public class Example1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("오늘은 목요일입니다.");
+		System.out.println("내일은 금요일ㅇ빈다.");
 	}
-
+	
+	
 }
